e13053e3b3eb3f98757b0f2f265372839a0bc968
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-30 00:55:17 -0600

    removing build

--- build/Routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Index_1 = require("../Controller/Index");
-const express = require('express');
-const router = express.Router({ mergeParams: true });
-router.route('/').get(Index_1.Index);
-module.exports = router;
--- build/Routes/traduccion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const traduccion_1 = require("../Controller/traduccion");
-const express = require('express');
-const router = express.Router({ mergeParams: true });
-router.route('/').post(traduccion_1.Traducir);
-module.exports = router;
